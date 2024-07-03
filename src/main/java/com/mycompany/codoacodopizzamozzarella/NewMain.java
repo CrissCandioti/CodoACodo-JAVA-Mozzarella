2574f78ab83a81abd461ea130528d34d7f44fc81
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-07-03 15:05:07 -0300

    .
    .

--- src/main/java/com/mycompany/codoacodopizzamozzarella/NewMain.java
+++ src/main/java/com/mycompany/codoacodopizzamozzarella/NewMain.java
@@ -4,7 +4,9 @@
  */
 package com.mycompany.codoacodopizzamozzarella;
 
+import Entidades.Cliente;
 import Entidades.Producto;
+import Persistence.ClienteDAO;
 import Persistence.ProductoDAO;
 
 /**
@@ -17,10 +19,9 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Producto p = new Producto("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fb3e9473-cd0d-43e0-ac42-519593a0f19d/d3f74ac-14415b13-a801-40c6-bdc6-7158dcf22e7e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ZiM2U5NDczLWNkMGQtNDNlMC1hYzQyLTUxOTU5M2EwZjE5ZFwvZDNmNzRhYy0xNDQxNWIxMy1hODAxLTQwYzYtYmRjNi03MTU4ZGNmMjJlN2UucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.N9fqaBnKwqrkmVbYll0nZFxiBjMtGZz7c1kY9MRIzjA", "Frijolito", "Frijol", "Luchador", 0);
-        ProductoDAO dao = new ProductoDAO();
-//        dao.persistirEntidad(p);
-//        System.out.println("Se agrego correctamente");
-System.out.println(dao.listaProductos());
+        Cliente c = new Cliente("Criss", "Candioi", "CrissC30", "criss@hotmail.com", "3312", "Esta buenarda la pizza de peperoni");
+        ClienteDAO dao = new ClienteDAO();
+        dao.persistirEntidad(c);
+        System.out.println("Se agrego correctamente");
     }
 }

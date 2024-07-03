02d48620fc352feb613c56a461562a3fe27700e7
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-07-03 18:10:03 -0300

    .
    .

--- target/generated-sources/annotations/Entidades/Producto_.java
+++ target/generated-sources/annotations/Entidades/Producto_.java
@@ -4,13 +4,14 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-07-03T15:18:21", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-07-03T18:06:34", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 
     public static volatile SingularAttribute<Producto, String> descripcion;
     public static volatile SingularAttribute<Producto, String> ingredientes;
     public static volatile SingularAttribute<Producto, Double> precio;
+    public static volatile SingularAttribute<Producto, String> categoria;
     public static volatile SingularAttribute<Producto, String> imagen;
     public static volatile SingularAttribute<Producto, Integer> id;
     public static volatile SingularAttribute<Producto, String> nombre;

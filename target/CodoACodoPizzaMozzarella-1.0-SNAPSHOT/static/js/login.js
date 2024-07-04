0ca5c11f1058fec5477a9f55d23ed0a01a057580
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-07-04 11:51:56 -0300

    .
    .

--- target/CodoACodoPizzaMozzarella-1.0-SNAPSHOT/static/js/login.js
+++ target/CodoACodoPizzaMozzarella-1.0-SNAPSHOT/static/js/login.js
@@ -1,8 +1,38 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    var email = document.getElementById('floatingInput').value;
-    var password = document.getElementById('floatingPassword').value;
-    if (email === '' || password === '') {
-      alert('Por favor, rellene ambos campos.');
-      event.preventDefault();
+// login.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('loginForm');
+
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+    // Obtener los valores del formulario
+    const email = document.getElementById('floatingInput').value;
+    const password = document.getElementById('floatingPassword').value;
+
+    try {
+      // Realizar la solicitud POST al servlet
+      const response = await fetch('http://localhost:8080/CodoACodoPizzaMozzarella/SvCliente/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ correoElectronico: email, contrasena: password })
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        // Si el inicio de sesión es exitoso, puedes redirigir al usuario o mostrar un mensaje
+        alert('Inicio de sesión exitoso');
+        // Redirigir a la página de inicio
+        window.location.href = '../index.html';
+      } else {
+        // Manejar el caso en que la solicitud falla
+        alert('Credenciales incorrectas');
+      }
+    } catch (error) {
+      console.error('Error en la solicitud de inicio de sesión:', error);
+      alert('Error en la solicitud de inicio de sesión');
     }
   });
+});
--- target/generated-sources/annotations/Entidades/Producto_.java
+++ target/generated-sources/annotations/Entidades/Producto_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-07-03T19:16:11", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-07-04T11:49:42", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 

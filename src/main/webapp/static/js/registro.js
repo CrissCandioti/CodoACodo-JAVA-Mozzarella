eb9468e998daf23ecaa22d7764df9d7bbee87f86
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-07-05 12:28:44 -0300

    .
    .

--- src/main/webapp/static/js/registro.js
+++ src/main/webapp/static/js/registro.js
@@ -1,1 +1,40 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    const registerForm = document.getElementById('registerForm');
+
+    registerForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const nombre = document.getElementById('floatingInputNombre').value;
+        const apellido = document.getElementById('floatingInputApellido').value;
+        const nombreDeUsuario = document.getElementById('floatingInputNombreUsuario').value;
+        const correoElectronico = document.getElementById('floatingInputCorreoElectronico').value;
+        const contrasena = document.getElementById('floatingPassword').value;
+
+        try {
+            const response = await fetch('http://localhost:8080/CodoACodoPizzaMozzarella/SvCliente/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nombre: nombre,
+                    apellido: apellido,
+                    nombreUsuario: nombreDeUsuario,
+                    correoElectronico: correoElectronico,
+                    contrasena: contrasena,
+                })
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                alert('Registro exitoso');
+            } else {
+                alert('Registro fallido');
+            }
+        } catch (error) {
+            console.error('Error en el registro del usuario');
+            alert('Error en el registro');
+        }
+    });
+});

f5138a195cb8f454022e7da0c0ed34a5ef7bb12b
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-07-05 18:33:10 -0300

    .
    .

---
+++ target/CodoACodoPizzaMozzarella-1.0-SNAPSHOT/static/js/contacto.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contactoForm = document.getElementById('contactoForm');
+    contactoForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const nombre = document.getElementById('floatingInputNombre').value;
+        const apellido = document.getElementById('floatingInputApellido').value;
+        const correoElectronico = document.getElementById('floatingInputCorreoElectronico').value;
+        const comentario = document.getElementById('FormControlTextarea').value;
+
+        try {
+            const response = await fetch('http://localhost:8080/CodoACodoPizzaMozzarella/SvCliente/', {
+                method: 'PUT',
+                header: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                 
+                    
+                    
+                    
+                    
+                })
+
+            }
+
+
+
+
+
+        } catch (e) {
+
+        }
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+

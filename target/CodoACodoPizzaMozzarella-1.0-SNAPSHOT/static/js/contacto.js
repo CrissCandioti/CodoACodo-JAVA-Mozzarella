eed6103b8370a78831297421d05877506db0e70b
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-07-08 17:44:01 -0300

    .
    .

--- target/CodoACodoPizzaMozzarella-1.0-SNAPSHOT/static/js/contacto.js
+++ target/CodoACodoPizzaMozzarella-1.0-SNAPSHOT/static/js/contacto.js
@@ -1,64 +1,49 @@
-//document.addEventListener('DOMContentLoaded', () => {
-//    const contactoForm = document.getElementById('contactoForm');
-//    contactoForm.addEventListener('submit', async (event) => {
-//        event.preventDefault();
-//
-//        const nombre = document.getElementById('floatingInputNombre').value;
-//        const apellido = document.getElementById('floatingInputApellido').value;
-//        const correoElectronico = document.getElementById('floatingInputCorreoElectronico').value;
-//        const comentario = document.getElementById('FormControlTextarea').value;
-//
-//        try {
-//            const response = await fetch('http://localhost:8080/CodoACodoPizzaMozzarella/SvCliente/', {
-//                method: 'PUT',
-//                header: {
-//                    'Content-Type': 'application/json'
-//                },
-//                body: JSON.stringify({
-//                 
-//                    
-//                    
-//                    
-//                    
-//                })
-//
-//            }
-//
-//
-//
-//
-//
-//        } catch (e) {
-//
-//        }
-//
-//
-//
-//
-//
-//
-//
-//    })
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//})
-//
+document.addEventListener('DOMContentLoaded', () => {
+    const contactoForm = document.getElementById('contactoForm');
+    contactoForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const nombre = document.getElementById('floatingInputNombre').value;
+        const apellido = document.getElementById('floatingInputApellido').value;
+        const correoElectronico = document.getElementById('floatingInputCorreoElectronico').value;
+        const comentario = document.getElementById('FormControlTextarea').value;
+
+        try {
+            const response = await fetch('http://localhost:8080/CodoACodoPizzaMozzarella/SvCliente/', {
+                method: 'PUT',
+                header: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    comengarios: comentario
+                })
+            });
+            alert('Se agrego el comentario en nuestra base de datos');
+        } catch (error) {
+            console.error('Error de red o de fetch', error);
+            alert('Error en el registro', error);
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+

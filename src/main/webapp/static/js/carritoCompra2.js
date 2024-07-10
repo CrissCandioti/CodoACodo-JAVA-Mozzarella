de5a180eb63c5165d7c15f35fac7843a6828925f
CrissCandioti <141766978+CrissCandioti@users.noreply.github.com>
2024-07-10 16:42:10 -0300

    .
    .

---
+++ src/main/webapp/static/js/carritoCompra2.js
@@ -0,0 +1,21 @@
+async function guardarCarritoEnBaseDeDatos() {
+    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    const usuario = localStorage.getItem("usuario") || ''; // Obtener el usuario del localStorage
+
+    try {
+        const response = await fetch('http://localhost:8080/CodoACodoPizzaMozzarella/SvCompra', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({usuario: usuario, productos: carrito})
+        });
+        alert('¡La compra se guardó correctamente!');
+        localStorage.removeItem("carrito"); 
+        window.location.href = '../index.html'; 
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error al guardar la compra');
+    }
+}
+guardarCarritoEnBaseDeDatos();
